b2a975dcbb94a791c8af8d5e636f9dbebe3ce115
Kimi Maulana <kimi93.com@gamil.com>
2024-12-01 17:54:06 +0700

    changes

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Home from './Home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      <Home />
+    </div>
+  );
 }
 
-export default App
+export default App;
