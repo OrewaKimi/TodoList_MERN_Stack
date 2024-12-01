5ab869e8a4bb7d413087ec52bdd86ffc366c0842
Kimi Maulana <kimi93.com@gamil.com>
2024-12-01 23:42:42 +0700

    Adding Data

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'; // Add useEffect here
 import Create from './Create';
+import axios from 'axios';
 
 function Home() {
     const [todos, setTodos] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:3001/get')
+        .then(result => setTodos(result.data))
+        .catch(err => console.log(err));
+    }, []);
+
     return (
-        <div>
-            <h2>Todo List</h2>
+        <div className='home'>
+            <h1>Todo List</h1>
             <Create />
             {
                 todos.length === 0 
                 ? 
-                <div><h2>No Record</h2></div>
+                <div><h2>No record</h2></div>
                 :
                 todos.map(todo => (
                     <div>
-                        {todo}
+                        {todo.task} 
                     </div>
                 ))
             }
