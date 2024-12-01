8d41c7c6c64a325acd50ce459e772ee856104e97
Kimi Maulana <kimi93.com@gamil.com>
2024-12-01 18:17:10 +0700

    Create File Create.Jsx

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Create from './Create';
 
 function Home() {
-  return (
-    <div>
-      <h1>Welcome to the Todo List App!</h1>
-    </div>
-  );
+    const [todos, setTodos] = useState([]);
+    return (
+        <div>
+            <h2>Todo List</h2>
+            <Create />
+            {
+                todos.length === 0 
+                ? 
+                <div><h2>No Record</h2></div>
+                :
+                todos.map(todo => (
+                    <div>
+                        {todo}
+                    </div>
+                ))
+            }
+        </div>
+    );
 }
 
 export default Home;
