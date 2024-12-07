bb5cbb7cf98771c8cb0e5a542955f0e6f8df91d1
OrewaKimi <118335496+OrewaKimi@users.noreply.github.com>
2024-12-07 13:39:40 +0700

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,54 +1,45 @@
-const express = require ('express')
-const mongoose  = require('mongoose')
-const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const TodoModel = require('./Models/Todo');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const app = express()
-app.use (cors())
-app.use(express.json())
+mongoose.connect('mongodb://127.0.0.1:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
-
-app.get('/get', (req, res) => {
-    TodoModel.find()
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
-
-app.put('/update/:id', (req, res) => {
-    const {id} = req.params;
-    TodoModel.findByIdAndUpdate({_id : id}, {done: true})
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
-
-app.delete('/delete/:id', (req, res) => {
-    const { id } = req.params;
-    console.log(`Request to delete ID: ${id}`);
-    TodoModel.findByIdAndDelete({ _id: id })
-    .then(result => {
-        if (result) {
-            res.json(result);
-        } else {
-            console.log('Todo not found');
-            res.status(404).json({ error: 'Todo not found' });
-        }
+app.route('/todos')
+    .get((req, res) => {
+        TodoModel.find()
+            .then(todos => res.json(todos))
+            .catch(err => res.status(500).json(err));
     })
-    .catch(err => {
-        console.error('Delete error:', err);
-        res.status(500).json(err);
+    .post((req, res) => {
+        const { task } = req.body;
+        TodoModel.create({ task })
+            .then(todo => res.status(201).json(todo))
+            .catch(err => res.status(500).json(err));
     });
-});
 
-app.post('/add', (req, res) => { 
-    const task = req.body.task;
-    TodoModel.create({
-        task: task
-    }).then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+app.route('/todos/:id')
+    .put((req, res) => {
+        TodoModel.findByIdAndUpdate(req.params.id, { done: true }, { new: true })
+            .then(todo => res.json(todo))
+            .catch(err => res.status(500).json(err));
+    })
+    .delete((req, res) => {
+        TodoModel.findByIdAndDelete(req.params.id)
+            .then(result => {
+                if (result) {
+                    res.json(result);
+                } else {
+                    res.status(404).json({ error: 'Todo not found' });
+                }
+            })
+            .catch(err => res.status(500).json(err));
+    });
 
 app.listen(3001, () => {
-    console.log('Server is running on port 3001')
-})
+    console.log('Server is running on port 3001');
+});
